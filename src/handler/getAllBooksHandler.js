6f6dee1c582ff84c4209436e85c561a402191355
username <abadisuryosetiyo@gmail.com>
2021-07-17 01:38:53 +0700

    change getAllBooksHandler function

--- src/handler/getAllBooksHandler.js
+++ src/handler/getAllBooksHandler.js
@@ -1,14 +1,57 @@
 const books = require('./books');
 
-const getAllBooksHandler = () => {
+const getAllBooksHandler = (request, h) => {
+  const {name, reading, finished} = request.query;
+
+  const cart = [];
+
+  // console.log('request.params ', request.query.name);
+  // console.log('name, reading, finished ', name, reading, finished);
+
+  if (name!==undefined) {
+    const filteredName = books.filter((item)=>
+      item.name.toLowerCase().includes(name.toLowerCase()));
+    // console.log('filteredName ', filteredName);
+    cart.push(...filteredName);
+  }
+  if (reading!==undefined) {
+    const filteredReading = reading == 1
+      ? books.filter((item)=>item.reading)
+      : reading == 0
+        ? books.filter((item)=>!item.reading)
+        : books;
+    cart.push(...filteredReading);
+  }
+  if (finished!==undefined) {
+    console.log('finished', finished);
+    const filteredFinished = finished == 1
+    ? books.filter((item)=> item.pageCount === item.readPage)
+    : finished == 0
+      ? books.filter((item)=> item.pageCount > item.readPage)
+      : books;
+    console.log('filteredFinished', filteredFinished);
+    cart.push(...filteredFinished);
+  }
+
   if (books.length>0) {
-    const data = books.map((item) => {
-      return ({
-        id: item.bookId,
-        name: item.name,
-        publisher: item.publisher,
-      });
-    });
+    const data = [];
+    if (name === undefined && reading == undefined && finished === undefined) {
+      data.push(...books.map((item) => {
+        return ({
+          id: item.bookId,
+          name: item.name,
+          publisher: item.publisher,
+        });
+      }));
+    } else {
+      data.push(...cart.map((item) => {
+        return ({
+          id: item.bookId,
+          name: item.name,
+          publisher: item.publisher,
+        });
+      }));
+    }
     // console.log(data);
     return {
       status: 'success',
