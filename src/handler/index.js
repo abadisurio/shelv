ae8dacd8bf5c7b266f097de504e0c42e92e5eb43
username <abadisuryosetiyo@gmail.com>
2021-07-16 22:12:45 +0700

    Handler: move handlers and notes to /handler

---
+++ src/handler/index.js
@@ -0,0 +1,13 @@
+const addNoteHandler = require('./addNoteHandler');
+const getAllNotesHandler = require('./getAllNotesHandler');
+const getNoteByIdHandler = require('./getNoteByIdHandler');
+const editNoteByIdHandler = require('./editNoteByIdHandler');
+const deleteNoteByIdHandler = require('./deleteNoteByIdHandler');
+
+module.exports = {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
+};
