4702fc1426d03b961c4c76e77de482a66c4ae096
username <abadisuryosetiyo@gmail.com>
2021-07-16 23:39:11 +0700

    change getAllBooksHandler function

--- src/handler/getAllBooksHandler.js
+++ src/handler/getAllBooksHandler.js
@@ -1,10 +1,29 @@
 const books = require('./books');
 
-const getAllBooksHandler = () => ({
-  status: 'success',
-  data: {
-    books,
-  },
-});
+const getAllBooksHandler = () => {
+  if (books.length>0) {
+    const data = books.map((item) => {
+      return ({
+        id: item.bookId,
+        name: item.name,
+        publisher: item.publisher,
+      });
+    });
+    console.log(data);
+    return {
+      status: 'success',
+      data: {
+        books: data,
+      },
+    };
+  }
+
+  return {
+    status: 'success',
+    data: {
+      books: [],
+    },
+  };
+};
 
 module.exports = getAllBooksHandler;
