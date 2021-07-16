f76bfa2298dd55ce71d861505d9a675ce2b5eac4
username <abadisuryosetiyo@gmail.com>
2021-07-16 23:12:05 +0700

    change addNoteHandler function

--- src/handler/addNoteHandler.js
+++ src/handler/addNoteHandler.js
@@ -2,20 +2,50 @@ const {nanoid} = require('nanoid');
 const notes = require('./notes');
 
 const addNoteHandler = (request, h) => {
-  const {title, tags, body} = request.payload;
-  const id = nanoid(16);
-  const createdAt = new Date().toISOString();
-  const updatedAt = createdAt;
-
+  const {name, year, author, summary, publisher, pageCount,
+    readPage, reading} = request.payload;
+  const bookId = nanoid(16);
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+  const finished = pageCount === readPage;
   const newNote = {
-    title, tags, body, id, createdAt, updatedAt,
+    name, year, author, summary, publisher, pageCount,
+    readPage, finished, reading, bookId, insertedAt, updatedAt,
   };
+
+  const checkError = [
+    {
+      condition: name === undefined,
+      code: 400,
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku',
+    },
+    {
+      condition: readPage > pageCount,
+      code: 400,
+      status: 'fail',
+      message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+    },
+  ];
+
+  const errors = checkError.filter((item) => item.condition);
+  if (errors.length > 0) {
+    error = errors[0];
+    console.log('condition: ', error.condition);
+    const response = h.response({
+      status: error.status,
+      message: error.message,
+    });
+    response.code(error.code);
+    return response;
+  }
+
   notes.push(newNote);
-  const isSuccess = notes.filter((note) => note.id === id).length > 0;
+  const isSuccess = notes.filter((note) => note.bookId === bookId).length > 0;
   if (isSuccess) {
     const response = h.response({
       status: 'success',
-      message: 'Catatan berhasil ditambahkan',
+      message: 'Buku berhasil ditambahkan',
       data: {
         ...newNote,
       },
@@ -23,9 +53,10 @@ const addNoteHandler = (request, h) => {
     response.code(201);
     return response;
   }
+
   const response = h.response({
-    status: 'fail',
-    message: 'Catatan gagal ditambahkan',
+    status: 'error',
+    message: 'Buku gagal ditambahkan',
   });
   response.code(500);
   return response;
